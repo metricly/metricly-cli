d2451c4202467a206bab7bf880b4f19b43dc4316
TheConnMan <bcconn2112@gmail.com>
2017-02-27 17:54:41 -0500

    Add package download URL check

--- validation/package.js
+++ validation/package.js
@@ -7,11 +7,22 @@ var indent = '';
 var Logger = require('../util/logger');
 var logger = new Logger(indent);
 
+var packageRepoRoot = 'https://github.com/netuitive-community-packages/';
+
 module.exports.validate = function(path, package, continueOnFinish) {
     var errorTracker = new ErrorTracker(package, indent);
 
     logger.log('------------------------------');
     logger.log('Package: ' + package);
+
+    try {
+      var manifest = JSON.parse(fs.readFileSync(path + '/package.json', 'utf8'));
+      var downloadUrl = manifest.manifest.distro.download;
+
+      errorTracker.assertEquals(downloadUrl,  packageRepoRoot + package.split('.').join('-') + '/archive/master.zip', 'Package download url is correct');
+    } catch (err) {
+      errorTracker.log('Error: ' + err);
+    }
 
     var hasDashboards = fs.existsSync(path + '/dashboards');
 
