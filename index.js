8dcec51dd35b2940c35b522e226ee839160c42ce
TheConnMan <bcconn2112@gmail.com>
2017-02-27 10:47:12 -0500

    Add widget ID mismatch output

--- index.js
+++ index.js
@@ -21,9 +21,17 @@ packages.forEach(package => {
       var widgetIds = dsb.dashboard.widgets.map(w => w.id).sort();
       var layoutIds = [].concat.apply([], JSON.parse(dsb.dashboard.layout).contents.map(c => c.widgets)).sort();
       console.log('    Old layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(layoutIds) ? '✓' : 'X'));
+      if (JSON.stringify(widgetIds) != JSON.stringify(layoutIds)) {
+        console.log('      Widget IDs: ' + JSON.stringify(widgetIds));
+        console.log('      Layout IDs: ' + JSON.stringify(layoutIds));
+      }
 
       var gridstackIds = gridstack.map(g => g.id).sort();
       console.log('    Gridstack layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(gridstackIds) ? '✓' : 'X'));
+      if (JSON.stringify(widgetIds) != JSON.stringify(layoutIds)) {
+        console.log('      Widget IDs: ' + JSON.stringify(widgetIds));
+        console.log('      Gridstack IDs: ' + JSON.stringify(gridstackIds));
+      }
     } catch(err) {
       console.log('    Error: ' + err);
     }
