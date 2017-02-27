239ebd40910422bc3708d2bfd572434ae31dc311
TheConnMan <bcconn2112@gmail.com>
2017-02-27 13:45:37 -0500

    Only check gridstack contents if it exists

--- index.js
+++ index.js
@@ -24,25 +24,28 @@ function checkDashboards(package) {
 
     try {
       var dsb = JSON.parse(fs.readFileSync(root + package + '/dashboards/' + dashboard, 'utf8'));
-      var contents = dsb.dashboard.properties.gridstackContents;
-      console.log('    Has gridstack: ' + (contents ? '✓' : 'X'));
-
-      var gridstack = JSON.parse(contents);
-      console.log('    Gridstack content is valid JSON: ✓');
 
       var widgetIds = dsb.dashboard.widgets.map(w => w.id).sort();
       var layoutIds = [].concat.apply([], JSON.parse(dsb.dashboard.layout).contents.map(c => c.widgets)).sort();
-      console.log('    Old layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(layoutIds) ? '✓' : 'X'));
+      console.log('    Layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(layoutIds) ? '✓' : 'X'));
       if (JSON.stringify(widgetIds) != JSON.stringify(layoutIds)) {
         console.log('      Widget IDs: ' + JSON.stringify(widgetIds));
         console.log('      Layout IDs: ' + JSON.stringify(layoutIds));
       }
 
-      var gridstackIds = gridstack.map(g => g.id).sort();
-      console.log('    Gridstack layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(gridstackIds) ? '✓' : 'X'));
-      if (JSON.stringify(widgetIds) != JSON.stringify(gridstackIds)) {
-        console.log('      Widget IDs: ' + JSON.stringify(widgetIds));
-        console.log('      Gridstack IDs: ' + JSON.stringify(gridstackIds));
+      var contents = dsb.dashboard.properties.gridstackContents;
+      console.log('    Has gridstack: ' + (contents ? '✓' : 'X'));
+
+      if (contents) {
+        var gridstack = JSON.parse(contents);
+        console.log('    Gridstack content is valid JSON: ✓');
+
+        var gridstackIds = gridstack.map(g => g.id).sort();
+        console.log('    Gridstack layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(gridstackIds) ? '✓' : 'X'));
+        if (JSON.stringify(widgetIds) != JSON.stringify(gridstackIds)) {
+          console.log('      Widget IDs: ' + JSON.stringify(widgetIds));
+          console.log('      Gridstack IDs: ' + JSON.stringify(gridstackIds));
+        }
       }
     } catch(err) {
       console.log('    Error: ' + err);
