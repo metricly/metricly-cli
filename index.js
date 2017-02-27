ac42b08c43ec5fd50cae710410e1c2dfa405ea08
TheConnMan <bcconn2112@gmail.com>
2017-02-27 10:33:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+const packages = fs.readdirSync('../').filter(f => fs.statSync('../' + f).isDirectory() && f.startsWith('netuitive-packages-'));
+
+packages.forEach(package => {
+  console.log('------------------------------');
+  console.log('Package: ' + package);
+
+  var dashboards = fs.readdirSync('../' + package + '/dashboards/');
+  dashboards.forEach(dashboard => {
+    console.log('  Dashboard: ' + dashboard);
+
+    try {
+      var dsb = JSON.parse(fs.readFileSync('../' + package + '/dashboards/' + dashboard, 'utf8'));
+      var contents = dsb.dashboard.properties.gridstackContents;
+      console.log('    Has gridstack: ' + (contents ? '✓' : 'X'));
+
+      var gridstack = JSON.parse(contents);
+      console.log('    Gridstack content is valid JSON: ✓');
+
+      var widgetIds = dsb.dashboard.widgets.map(w => w.id).sort();
+      var layoutIds = [].concat.apply([], JSON.parse(dsb.dashboard.layout).contents.map(c => c.widgets)).sort();
+      console.log('    Old layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(layoutIds) ? '✓' : 'X'));
+
+      var gridstackIds = gridstack.map(g => g.id).sort();
+      console.log('    Gridstack layout IDs match widget IDs: ' + (JSON.stringify(widgetIds) == JSON.stringify(gridstackIds) ? '✓' : 'X'));
+    } catch(err) {
+      console.log('    Error: ' + err);
+    }
+  });
+});
