99567e09a6b5ecdc7ac61163c9b5ad0057844166
TheConnMan <bcconn2112@gmail.com>
2017-08-13 15:58:43 -0400

    Add package deployment request

--- deploy/index.js
+++ deploy/index.js
@@ -1,8 +1,20 @@
 var zipFolder = new require('zip-folder');
+var fs = require('fs');
+var request = require('request-promise');
 
 module.exports = function(config, logger, location) {
   logger.info('\nDeploying package');
-  return createZip(location).then((zip) => {
+  return createZip(location).then(zip => {
+    return request.post({
+      uri: config.endpoint + '/packages',
+      qs: {
+        tenantId: config.tenantId
+      },
+      formData: {
+        file: fs.createReadStream(zip)
+      }
+    });
+  }).then((body) => {
     logger.info('\nDone deploying');
   });
 };
