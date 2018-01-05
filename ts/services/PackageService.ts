772be81fd82d37931bf722c4d149814947876cfd
TheConnMan <bcconn2112@gmail.com>
2018-01-05 11:55:08 -0500

    Clean up PackageService with async/await

--- ts/services/PackageService.ts
+++ ts/services/PackageService.ts
@@ -4,76 +4,80 @@ import * as Bluebird from 'bluebird';
 
 class PackageService {
 
-  public listInstalled(config, logger): Bluebird<void> {
+  public async listInstalled(config, logger): Promise<void> {
     logger.debug('\nListing installed packages');
-    return request({
-      uri: config.endpoint + '/packages',
-      auth: {
-        user: config.username,
-        pass: config.password
-      }
-    }).then(body => {
+    try {
+      const body = await request({
+        uri: config.endpoint + '/packages',
+        auth: {
+          user: config.username,
+          pass: config.password
+        }
+      });
       logger.info('The following packages are installed:');
       logger.info(JSON.parse(body).packages.sort((pkg1, pkg2) => {
         return pkg1.name.localeCompare(pkg2.name);
       }).map(pkg => {
         return pkg.name + ':v' + pkg.version + ' (ID: ' + pkg.id + ')';
       }));
-    }).catch(e => {
+    } catch (e) {
       logger.error('There was an error listing the packages');
-    });
+    }
   }
 
-  public getById(id: string, config, logger) {
+  public async getById(id: string, config, logger): Promise<void> {
     logger.debug('\nGetting package ' + id);
-    return request({
-      uri: config.endpoint + '/packages/' + id,
-      auth: {
-        user: config.username,
-        pass: config.password
-      }
-    }).then(body => {
+    try {
+      const body = await request({
+        uri: config.endpoint + '/packages/' + id,
+        auth: {
+          user: config.username,
+          pass: config.password
+        }
+      });
       var pkg = JSON.parse(body).package;
       logger.info(JSON.stringify(pkg, null, 2));
-    }).catch(e => {
+    } catch (e) {
       logger.error('There was an error getting the package');
-    });
+    }
   }
 
-  public installFromUrl(url: string, config, logger): Bluebird<void> {
+  public async installFromUrl(url: string, config, logger): Promise<void> {
     logger.debug('\nInstalling package from ' + url);
-    return request.post({
-      uri: config.endpoint + '/packages/',
-      auth: {
-        user: config.username,
-        pass: config.password
-      },
-      json: {
-        archives: [url]
-      },
-      qs: {
-        userEmail: config.username
-      }
-    }).then(body => {
+    try {
+      const body = await request.post({
+        uri: config.endpoint + '/packages/',
+        auth: {
+          user: config.username,
+          pass: config.password
+        },
+        json: {
+          archives: [url]
+        },
+        qs: {
+          userEmail: config.username
+        }
+      });
       logger.info('Successfully installed package, ID: ' + body.packages[0].id);
-    }).catch(e => {
+    } catch (e) {
       logger.error('There was an error installing the package');
-    });
+    }
   }
 
-  public uninstallById(id: string, config, logger): Bluebird<void> {
+  public async uninstallById(id: string, config, logger): Promise<void> {
     logger.debug('\nUninstalling package ' + id);
-    return request.delete({
-      uri: config.endpoint + '/packages/' + id,
-      auth: {
-        user: config.username,
-        pass: config.password
-      }
-    }).then(body => {
+    try {
+      const body = await request.delete({
+        uri: config.endpoint + '/packages/' + id,
+        auth: {
+          user: config.username,
+          pass: config.password
+        }
+      });
       logger.info('Successfully uninstalled package ' + id);
-    }).catch(e => {
+    } catch (e) {
       logger.error('There was an error uninstalling the package');
-    });
+    }
   }
 }
 
