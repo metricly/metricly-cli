2a64b45dcbee3f181c781f7deebf16ef543a5b7f
TheConnMan <bcconn2112@gmail.com>
2018-01-11 17:45:59 -0500

    Add check version command
    Closes #10

--- ts/bin/metricly.ts
+++ ts/bin/metricly.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 import * as caporal from 'caporal';
+import * as request from 'request-promise';
 
 import ConfigCommands from '../commands/ConfigCommands';
 import DashboardCommands from '../commands/DashboardCommands';
@@ -14,6 +15,26 @@ const pkg = require('../../package.json');
 (caporal as any)
   .version(pkg.version)
   .description(pkg.description);
+
+(caporal as any)
+  .command('check version', 'Check if there is a newer version of the Metricly CLI')
+  .action(async (args, options, logger) => {
+    const release = await request({
+      headers: {
+        'User-Agent': 'Metricly-CLI'
+      },
+      json: true,
+      uri: 'https://api.github.com/repos/metricly/metricly-cli/releases/latest'
+    });
+    if (`v${pkg.version}` !== release.tag_name) {
+      // tslint:disable-next-line:max-line-length
+      logger.info('There is a updated version available. Install with *npm i -g metricly-cli* or download a binary from https://github.com/metricly/metricly-cli/releases/latest');
+    } else {
+      logger.info('You have the latest version.');
+    }
+    logger.info(`\nCurrent version: v${pkg.version}`);
+    logger.info(`Latest version:  ${release.tag_name}`);
+  });
 
 ConfigCommands.addCommands();
 PackageCommands.addCommands();
