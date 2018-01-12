3442bd3d6195a006c8c4fae371c45c2c10d64cde
TheConnMan <bcconn2112@gmail.com>
2018-01-12 09:27:53 -0500

    Add additional config util methods

--- ts/util/CommandUtils.ts
+++ ts/util/CommandUtils.ts
@@ -3,10 +3,19 @@ import * as fs from 'fs';
 
 class CommandUtils {
 
-  public static mergeConfig(options) {
+  public static getConfig() {
     const location = process.env.HOME + '/.metricly-cli.json';
-    const config = fs.existsSync(location) ? JSON.parse((fs.readFileSync(location).toString())) : {};
+    return fs.existsSync(location) ? JSON.parse((fs.readFileSync(location).toString())) : {};
+  }
+
+  public static mergeConfig(options) {
+    const config = this.getConfig();
     return extend({}, config, options);
+  }
+
+  public static saveConfig(config) {
+    const location = process.env.HOME + '/.metricly-cli.json';
+    fs.writeFileSync(location, JSON.stringify(config, null, 2));
   }
 }
 
