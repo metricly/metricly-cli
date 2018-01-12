a7680d52289d8371e96bb4db95ef9baf7a598baf
TheConnMan <bcconn2112@gmail.com>
2018-01-12 09:06:50 -0500

    Refactor Version commands to a separate class

--- ts/bin/metricly.ts
+++ ts/bin/metricly.ts
@@ -1,12 +1,12 @@
 #! /usr/bin/env node
 
 import * as caporal from 'caporal';
-import * as request from 'request-promise';
 
 import ConfigCommands from '../commands/ConfigCommands';
 import DashboardCommands from '../commands/DashboardCommands';
 import PackageCommands from '../commands/PackageCommands';
 import PolicyCommands from '../commands/PolicyCommands';
+import VersionCommands from '../commands/VersionCommands';
 import CommandUtils from '../util/CommandUtils';
 
 // tslint:disable-next-line:no-var-requires
@@ -16,29 +16,9 @@ const pkg = require('../../package.json');
   .version(pkg.version)
   .description(pkg.description);
 
-(caporal as any)
-  .command('check version', 'Check if there is a newer version of the Metricly CLI')
-  .action(async (args, options, logger) => {
-    const release = await request({
-      headers: {
-        'User-Agent': 'Metricly-CLI'
-      },
-      json: true,
-      uri: 'https://api.github.com/repos/metricly/metricly-cli/releases/latest'
-    });
-    if (`v${pkg.version}` !== release.tag_name) {
-      // tslint:disable-next-line:max-line-length
-      logger.info('There is a updated version available. Install with *npm i -g metricly-cli* or download a binary from https://github.com/metricly/metricly-cli/releases/latest');
-    } else {
-      logger.info('You have the latest version.');
-    }
-    logger.info(`\nCurrent version: v${pkg.version}`);
-    logger.info(`Latest version:  ${release.tag_name}`);
-  });
-
-ConfigCommands.addCommands();
 PackageCommands.addCommands();
 PolicyCommands.addCommands();
 DashboardCommands.addCommands();
+VersionCommands.addCommands();
 
 (caporal as any).parse(process.argv);
