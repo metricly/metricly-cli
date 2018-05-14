2489a980f6de64a1efb80ce2d4651cfb5a6433e2
TheConnMan <bcconn2112@gmail.com>
2018-05-14 11:14:38 -0400

    Implement Policy create, update, and delete
    Closes #45

--- ts/services/PolicyService.ts
+++ ts/services/PolicyService.ts
@@ -51,6 +51,61 @@ class PolicyService {
       logger.error('There was an error getting the policy: ' + e);
     }
   }
+
+  public async create(file: string, config, logger): Promise<void> {
+    logger.debug('\nCreating policy');
+    try {
+      const policy = JSON.parse(fs.readFileSync(file, 'utf-8'));
+      const response = await request({
+        auth: {
+          pass: config.password,
+          user: config.username
+        },
+        body: policy,
+        json: true,
+        method: 'POST',
+        uri: config.endpoint + '/policies'
+      });
+    } catch (e) {
+      logger.error('There was an error creating the policy: ' + e);
+    }
+  }
+
+  public async update(id: string, file: string, config, logger): Promise<void> {
+    logger.debug('\nUpdating policy ' + id);
+    try {
+      const policy = JSON.parse(fs.readFileSync(file, 'utf-8'));
+      const response = await request({
+        auth: {
+          pass: config.password,
+          user: config.username
+        },
+        body: policy,
+        json: true,
+        method: 'PUT',
+        uri: config.endpoint + '/policies/' + id
+      });
+    } catch (e) {
+      logger.error('There was an error updating the policy: ' + e);
+    }
+  }
+
+  public async deleteById(id: string, config, logger): Promise<void> {
+    logger.debug('\nDeleting policy ' + id);
+    try {
+      const response = await request({
+        auth: {
+          pass: config.password,
+          user: config.username
+        },
+        json: true,
+        method: 'DELETE',
+        uri: config.endpoint + '/policies/' + id
+      });
+    } catch (e) {
+      logger.error('There was an error deleting the policy: ' + e);
+    }
+  }
 }
 
 export default PolicyService;
